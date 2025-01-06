41bdc0b98714af1a5c29a3af6e34a9fd44e6af3b
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-06 23:52:11 +0900

    feat: floating button to toggle storybook ui

--- App.tsx
+++ App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
-// import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import StoryBookUI from './.storybook';
 
 export default function App() {
-  return <StoryBookUI />;
-  // return <Text>App</Text>;
+  const [storybookEnabled, setStorybookEnabled] = useState(false);
+
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => setStorybookEnabled(prev => !prev)}
+        activeOpacity={0.8}
+        style={{
+          position: 'absolute',
+          right: 30,
+          bottom: 30,
+          zIndex: 1000,
+          width: 60,
+          height: 60,
+          borderRadius: '50%',
+          backgroundColor: 'purple',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 24,
+          }}>
+          S
+        </Text>
+      </TouchableOpacity>
+      {storybookEnabled ? (
+        <StoryBookUI />
+      ) : (
+        <SafeAreaView>
+          <Text>App</Text>
+        </SafeAreaView>
+      )}
+    </>
+  );
 }
