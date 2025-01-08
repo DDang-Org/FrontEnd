2e7556547412d33eb93977f6e5be1c58d1a56093
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-08 21:55:44 +0900

    chore: 절대경로 설정 - babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '~apis': './src/apis',
+          '~assets': './src/assets',
+          '~components': './src/components',
+          '~constants': './src/constants',
+          '~hooks': './src/hooks',
+          '~navigation': './src/navigation',
+          '~screens': './src/screens',
+          '~styles': './src/styles',
+          '~types': './src/types',
+          '~': '.',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
