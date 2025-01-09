190269fedfc757e8d7eff20ee359a3324bd236cd
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-09 14:19:43 +0900

    refactor: App.tsx 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from '@emotion/native';
+import styled from '@emotion/native';
 import { ThemeProvider } from '@emotion/react';
 import { NavigationContainer } from '@react-navigation/native';
 import { useState } from 'react';
@@ -9,56 +9,54 @@ import { lightTheme } from '~styles/theme';
 import StoryBookUI from '../.storybook';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const StoryBookFloatingButton = styled.TouchableOpacity<{ visible: boolean }>`
+const StoryBookFloatingButton = styled.TouchableOpacity`
   position: absolute;
   right: 30px;
   bottom: 30px;
   z-index: 1000;
   width: 60px;
   height: 60px;
-  border-radius: 50%;
+  border-radius: 30px;
   background-color: purple;
   align-items: center;
   justify-content: center;
-  display: ${({ visible }) => (visible ? 'unset' : 'none')};
+`;
+
+const StoryBookButtonText = styled.Text`
+  color: white;
+  font-size: 24px;
 `;
 
 const queryClient = new QueryClient();
+
 export default function App() {
   const { isMswEnabled } = useInitializeMsw();
   const [storybookEnabled, setStorybookEnabled] = useState(false);
-  const onPress = () => setStorybookEnabled(prev => !prev);
 
   if (__DEV__ && !isMswEnabled) {
     return <Text>Loading MSW...</Text>;
   }
 
+  const toggleStorybook = () => setStorybookEnabled(prev => !prev);
+
+  const MainApp = () => (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={lightTheme}>
+        <NavigationContainer>
+          <BottomTabNavigator />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+
   return (
     <>
       {__DEV__ && (
-        <StoryBookFloatingButton onPress={onPress} activeOpacity={0.8} visible={true}>
-          <Text
-            style={css`
-              color: white;
-              font-size: 24px;
-            `}
-          >
-            S
-          </Text>
+        <StoryBookFloatingButton onPress={toggleStorybook} activeOpacity={0.8}>
+          <StoryBookButtonText>S</StoryBookButtonText>
         </StoryBookFloatingButton>
       )}
-
-      {__DEV__ && storybookEnabled ? (
-        <StoryBookUI />
-      ) : (
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider theme={lightTheme}>
-            <NavigationContainer>
-              <BottomTabNavigator />
-            </NavigationContainer>
-          </ThemeProvider>
-        </QueryClientProvider>
-      )}
+      {__DEV__ && storybookEnabled ? <StoryBookUI /> : <MainApp />}
     </>
   );
 }
