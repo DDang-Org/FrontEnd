4ca882442eb1ac56a75a174c745fac4080c2f518
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-09 12:34:51 +0900

    refactor: MSW handlers MOCK_URL

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,9 @@
 import { http, HttpResponse } from 'msw';
 
+const MOCK_URL = 'https://api.example.com';
+
 export const handlers = [
-  http.get('https://example.com/user', () => {
+  http.get(`${MOCK_URL}/user`, () => {
     return HttpResponse.json({
       id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
       firstName: 'John',
