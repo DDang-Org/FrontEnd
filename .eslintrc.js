775818a615f891bbf2d0bab396aaddf7fc13b3dc
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-09 14:45:47 +0900

    chore: eslint tanstack query rules 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:@tanstack/query/recommended'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/no-rest-destructuring': 'error',
+    '@tanstack/query/stable-query-client': 'error',
+    '@tanstack/query/no-unstable-deps': 'error',
+    '@tanstack/query/infinite-query-property-order': 'error',
   },
-  extends: ['plugin:@tanstack/query/recommended'],
 };
