23c5196980f5acdaa921f3c75f55f05437003dbe
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-09 15:30:22 +0900

    docs: BottomTabNavigator 경고 해제 주석 추가
    - 컴포넌트는 알맞게 작성되었으나, eslint에서 불필요한 경고문이 뜨는 것으로 판단

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unstable-nested-components */
+
 import { css } from '@emotion/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { View } from 'react-native';
