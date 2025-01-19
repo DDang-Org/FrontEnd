d622dbc1b94908e4dc63b9592dfa4a22826e3402
shlee9999 <oklshop555@naver.com>
2025-01-19 17:44:56 +0900

    fix: Notification Header

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@emotion/react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '~screens/Home';
 import { NotificationScreen } from '~screens/Home/Notification';
@@ -12,14 +13,29 @@ export type HomeStackProps = {
 const Stack = createNativeStackNavigator<HomeStackProps>();
 
 export const HomeNavigator = () => {
+  const theme = useTheme();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackButtonDisplayMode: 'minimal',
+        headerTintColor: theme.colors.font_1,
+        headerShadowVisible: false,
+        headerTitleAlign: 'center',
+        headerBackButtonMenuEnabled: true,
+        headerTitleStyle: {
+          fontFamily: 'SUIT-Bold',
+          fontSize: 18,
+        },
+      }}
+    >
       <Stack.Screen name="Main" component={HomeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Walk" component={WalkScreen} options={{ headerBackButtonDisplayMode: 'minimal' }} />
       <Stack.Screen
         name="Notification"
         component={NotificationScreen}
-        options={{ headerBackButtonDisplayMode: 'minimal', title: '알림' }}
+        options={{
+          title: '알림',
+        }}
       />
     </Stack.Navigator>
   );
