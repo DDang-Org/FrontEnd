78db37c74df95a4fc0de2677893ab56ffeaa706e
wonil <nwi6959@gmail.com>
2025-01-21 13:26:39 +0900

    refactor: 불필요한 코드 삭제

--- src/components/Common/MultilineInput/MultilineInput.stories.tsx
+++ src/components/Common/MultilineInput/MultilineInput.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { View } from 'react-native';
 import { MultilineInput, MultilineInputProps } from './index';
