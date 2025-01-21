b5ab316ed5ad04a935ed9852309e8a3cf658d29d
GYU HAN <ruehan98@gmail.com>
2025-01-21 13:53:24 +0900

    feat: 스타일 분리

--- src/components/Common/WalkModal/styles.ts
+++ src/components/Common/WalkModal/styles.ts
@@ -1,5 +1,10 @@
 import styled from '@emotion/native';
-import { ActionButton } from '~components/Common/ActionButton';
+import { ResizeButton } from '~components/Common/ResizeButton';
+import { TextBold } from '~components/Common/Text';
+
+interface ButtonContainerProps {
+  type: 'walkStart' | 'walkJoin' | 'walkCancel' | 'report' | 'reportComplete' | 'walkComplete';
+}
 
 export const ModalBackground = styled.TouchableOpacity`
   flex: 1;
@@ -22,11 +27,10 @@ export const TitleContainer = styled.View`
   justify-content: center;
   align-items: center;
   text-align: center;
+  position: relative;
 `;
 
-export const Title = styled.Text`
-  font-size: 16px;
-  font-weight: bold;
+export const Title = styled(TextBold)`
   margin-bottom: 16px;
   text-align: center;
 `;
@@ -48,35 +52,32 @@ export const Input = styled.TextInput`
   color: ${props => props.theme.colors.font_3};
 `;
 
-export const ButtonContainer = styled.View`
+export const ButtonContainer = styled.View<ButtonContainerProps>`
   justify-content: center;
-  gap: 10px;
+  gap: 8px;
   margin-top: 20px;
   width: 100%;
   flex-direction: row;
 `;
 
-export const Button = styled.TouchableOpacity<{ variant?: 'primary' | 'secondary' }>`
-  padding: 12px 24px;
-  border-radius: 8px;
-  flex: 1;
-  background-color: ${props => (props.variant === 'primary' ? '#8B4513' : '#F5F5F5')};
-`;
+export const ModalButton = styled(ResizeButton)``;
 
-export const ButtonText = styled.Text<{ variant?: 'primary' | 'secondary' }>`
-  color: ${props => (props.variant === 'primary' ? 'white' : 'black')};
+export const ButtonText = styled(TextBold)`
   text-align: center;
-  font-size: 14px;
 `;
 
-export const Message = styled.Text`
-  font-size: 14px;
-  color: #666;
+export const MessageContainer = styled.View`
+  background-color: ${props => props.theme.colors.gc_3};
+  width: 100%;
+  padding: 19px;
+  border-radius: 12px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Message = styled(TextBold)`
+  color: ${props => props.theme.colors.font_1};
   text-align: center;
   margin-bottom: 16px;
-`;
-
-export const ModalButton = styled(ActionButton)`
-  max-width: 280px;
-  width: 100%;
 `;
