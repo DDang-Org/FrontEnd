7459293989b94ec052e005644d949afe95ce4d39
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-21 13:34:37 +0900

    feat: ToggleItem enabled prop

--- src/components/MyPage/Setting/ToggleBox/index.tsx
+++ src/components/MyPage/Setting/ToggleBox/index.tsx
@@ -6,14 +6,18 @@ export const ToggleBox = ({ children }: PropsWithChildren) => {
   return <S.ToggleBox>{children}</S.ToggleBox>;
 };
 
-export const Item = ({ title, description }: PropsWithChildren<{ title: string; description?: string }>) => {
+export const Item = ({
+  title,
+  description,
+  enabled,
+}: PropsWithChildren<{ title: string; description?: string; enabled: boolean }>) => {
   return (
     <S.Item paddingHorizontal={20} paddingVertical={16.5}>
       <S.TypoWrapper>
         <TextBold fontSize={17}>{title}</TextBold>
         {description && <TextRegular fontSize={15}>{description}</TextRegular>}
       </S.TypoWrapper>
-      <S.Toggle />
+      <S.Toggle value={enabled} />
     </S.Item>
   );
 };
