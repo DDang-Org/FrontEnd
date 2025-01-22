122306c2143f9bbfd5dba25bcb515c09ec0d9d61
GYU HAN <ruehan98@gmail.com>
2025-01-22 12:56:28 +0900

    fix: 스타일 분리

--- src/components/Common/ListModal/index.tsx
+++ src/components/Common/ListModal/index.tsx
@@ -1,9 +1,7 @@
-import styled from '@emotion/native';
 import { Modal, ScrollView, Animated, Dimensions, GestureResponderEvent } from 'react-native';
 import { useEffect, useRef } from 'react';
-import { ResizeButton } from '~components/Common/ResizeButton';
-import { TextBold, TextMedium } from '~components/Common/Text';
 import { Separator } from '~components/Common/Seperator';
+import * as S from './styles';
 
 interface DogListModalProps {
   isVisible: boolean;
@@ -32,75 +30,6 @@ interface DogListModalProps {
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 const WALK_MODAL_HEIGHT = SCREEN_HEIGHT * 0.85;
 
-const ModalBackground = styled.TouchableOpacity`
-  flex: 1;
-  background-color: rgba(0, 0, 0, 0.5);
-`;
-
-const ModalContainer = styled(Animated.View)<{ type: 'walk' | 'default' }>`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  min-height: ${props => (props.type === 'walk' ? `${WALK_MODAL_HEIGHT}px` : 'auto')};
-  background-color: white;
-  border-top-left-radius: 20px;
-  border-top-right-radius: 20px;
-`;
-
-const HeaderContainer = styled.View`
-  padding: 20px;
-  border-bottom-width: 1px;
-  border-bottom-color: #eee;
-`;
-
-const Title = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  text-align: center;
-`;
-
-const DogItem = styled.View`
-  flex-direction: row;
-  padding: 15px;
-  align-items: center;
-  border-bottom-width: 1px;
-  border-bottom-color: #eee;
-`;
-
-const DogImage = styled.Image`
-  width: 64px;
-  height: 64px;
-  border-radius: 1000px;
-`;
-
-const DogInfo = styled.View`
-  flex: 1;
-  margin-left: 15px;
-  margin-right: 8px;
-  gap: 4px;
-  flex-direction: column;
-`;
-
-const ButtonContainer = styled.View`
-  justify-content: center;
-  align-self: center;
-`;
-
-const NameText = styled(TextBold)``;
-
-const InfoText = styled(TextMedium)``;
-
-const InfoContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  gap: 4px;
-`;
-
-const WalkText = styled(TextBold)``;
-
-const SelectButton = styled(ResizeButton)``;
-
 export const DogListModal = ({ isVisible, onClose, dogs, onSelectDog, type = 'default' }: DogListModalProps) => {
   const slideAnim = useRef(new Animated.Value(type === 'walk' ? WALK_MODAL_HEIGHT : 500)).current;
 
@@ -125,8 +54,8 @@ export const DogListModal = ({ isVisible, onClose, dogs, onSelectDog, type = 'de
 
   return (
     <Modal visible={isVisible} transparent={true} onRequestClose={onClose} animationType="fade">
-      <ModalBackground activeOpacity={1} onPress={onClose}>
-        <ModalContainer
+      <S.ModalBackground activeOpacity={1} onPress={onClose}>
+        <S.ModalContainer
           type={type}
           style={{
             transform: [{ translateY: slideAnim }],
@@ -134,42 +63,42 @@ export const DogListModal = ({ isVisible, onClose, dogs, onSelectDog, type = 'de
           onStartShouldSetResponder={() => true}
           onTouchEnd={(e: GestureResponderEvent) => e.stopPropagation()}
         >
-          <HeaderContainer>
-            <Title>{type === 'walk' ? '강번따 리스트' : '강아지 리스트'}</Title>
-          </HeaderContainer>
+          <S.HeaderContainer>
+            <S.Title>{type === 'walk' ? '강번따 리스트' : '강아지 리스트'}</S.Title>
+          </S.HeaderContainer>
 
           <ScrollView>
             {dogs.map(dog => (
-              <DogItem key={dog.id}>
-                <DogImage
+              <S.DogItem key={dog.id}>
+                <S.DogImage
                   source={{
                     uri: dog.imageUrl ? dog.imageUrl : '',
                   }}
                 />
-                <DogInfo>
-                  <NameText fontSize={17}>{dog.name}</NameText>
-                  <InfoContainer>
-                    <InfoText fontSize={14}>{dog.breed}</InfoText>
+                <S.DogInfo>
+                  <S.NameText fontSize={17}>{dog.name}</S.NameText>
+                  <S.InfoContainer>
+                    <S.InfoText fontSize={14}>{dog.breed}</S.InfoText>
                     <Separator $height={14} />
-                    <InfoText fontSize={14}>{dog.age}</InfoText>
+                    <S.InfoText fontSize={14}>{dog.age}</S.InfoText>
                     <Separator $height={14} />
-                    <InfoText fontSize={14}>{dog.gender}</InfoText>
-                  </InfoContainer>
-                  <WalkText fontSize={14}>산책 횟수 {dog.walkCount}회</WalkText>
-                </DogInfo>
-                <ButtonContainer>
-                  <SelectButton
+                    <S.InfoText fontSize={14}>{dog.gender}</S.InfoText>
+                  </S.InfoContainer>
+                  <S.WalkText fontSize={14}>산책 횟수 {dog.walkCount}회</S.WalkText>
+                </S.DogInfo>
+                <S.ButtonContainer>
+                  <S.SelectButton
                     onPress={() => onSelectDog(dog)}
                     type="roundedRect"
                     bgColor="lighten_3"
                     text={type === 'walk' ? '강번따' : '추가'}
                   />
-                </ButtonContainer>
-              </DogItem>
+                </S.ButtonContainer>
+              </S.DogItem>
             ))}
           </ScrollView>
-        </ModalContainer>
-      </ModalBackground>
+        </S.ModalContainer>
+      </S.ModalBackground>
     </Modal>
   );
 };
