44c15c8fcc37c7d25671906b870db785cbe7e894
kimjuyoung99 <jjassb404@naver.com>
2025-01-22 14:18:30 +0900

    fix : Profile 컴포넌트 코드 수정

--- src/components/Common/Profile/index.tsx
+++ src/components/Common/Profile/index.tsx
@@ -3,10 +3,10 @@ import { TouchableOpacity, Image } from 'react-native';
 import { SvgProps } from 'react-native-svg';
 
 type ProfileProps = {
-  size: number;
-  src: string | React.FC<SvgProps>;
-  userId?: number;
-  testID?: string;  // (테스트 코드용)testID를 옵셔널 prop으로 추가
+    size: number;
+    src: string | React.FC<SvgProps>;
+    userId?: number;
+    testID?: string;  // (테스트 코드용)testID를 옵셔널 prop으로 추가
 };
 
 export default function Profile({ 
@@ -14,22 +14,22 @@ export default function Profile({
     src, 
     userId,
     testID 
-  }: ProfileProps) {
-    const renderImage = () => {
-      if (typeof src === 'string') {
-        return <Image source={{ uri: src }} style={{ width: size, height: size }} />;
-      }
-      const SvgComponent = src;
-      return <SvgComponent width={size} height={size} />;
-    };
-  
-    return (
-      <TouchableOpacity 
-        activeOpacity={userId ? 0.8 : 1} 
-        disabled={!userId}
-        testID={testID}
-      >
-        {renderImage()}
-      </TouchableOpacity>
-    );
-  }
+    }: ProfileProps) {
+        const renderImage = () => {
+        if (typeof src === 'string') {
+            return <Image source={{ uri: src }} style={{ width: size, height: size }} />;
+        }
+        const SvgComponent = src;
+        return <SvgComponent width={size} height={size} />;
+        };
+    
+        return (
+        <TouchableOpacity 
+            activeOpacity={userId ? 0.8 : 1} 
+            disabled={!userId}
+            testID={testID}
+        >
+            {renderImage()}
+        </TouchableOpacity>
+        );
+    }
