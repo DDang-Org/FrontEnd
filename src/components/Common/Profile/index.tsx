fb9dc0bd6ddba99b9a5e8bb8e1291eb60b038124
kimjuyoung99 <jjassb404@naver.com>
2025-01-22 14:13:25 +0900

    feat : Profile 컴포넌트 test code 추가

--- src/components/Common/Profile/index.tsx
+++ src/components/Common/Profile/index.tsx
@@ -6,23 +6,30 @@ type ProfileProps = {
   size: number;
   src: string | React.FC<SvgProps>;
   userId?: number;
+  testID?: string;  // (테스트 코드용)testID를 옵셔널 prop으로 추가
 };
 
-export default function Profile({ size, src, userId }: ProfileProps) {
-  const renderImage = () => {
-    if (typeof src === 'string') {
-      return <Image source={{ uri: src }} style={{ width: size, height: size }} />;
-    }
-    const SvgComponent = src;
-    return <SvgComponent width={size} height={size} />;
-  };
-
-  return (
-    <TouchableOpacity 
-      activeOpacity={userId ? 0.8 : 1} 
-      disabled={!userId}
-    >
-      {renderImage()}
-    </TouchableOpacity>
-  );
-}
+export default function Profile({ 
+    size, 
+    src, 
+    userId,
+    testID 
+  }: ProfileProps) {
+    const renderImage = () => {
+      if (typeof src === 'string') {
+        return <Image source={{ uri: src }} style={{ width: size, height: size }} />;
+      }
+      const SvgComponent = src;
+      return <SvgComponent width={size} height={size} />;
+    };
+  
+    return (
+      <TouchableOpacity 
+        activeOpacity={userId ? 0.8 : 1} 
+        disabled={!userId}
+        testID={testID}
+      >
+        {renderImage()}
+      </TouchableOpacity>
+    );
+  }
