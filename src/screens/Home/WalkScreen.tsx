9df4009147fa3d65f0efb607d080f199fb0cc51a
GYU HAN <ruehan98@gmail.com>
2025-01-22 14:26:29 +0900

    feat: 헤더 그라데이션 추가

--- src/screens/Home/WalkScreen.tsx
+++ src/screens/Home/WalkScreen.tsx
@@ -2,9 +2,11 @@ import { NaverMapView } from '@mj-studio/react-native-naver-map';
 import { useNavigation } from '@react-navigation/native';
 import { useLayoutEffect } from 'react';
 import * as S from './walk-styles';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const WalkScreen = () => {
   const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -14,14 +16,21 @@ export const WalkScreen = () => {
 
   return (
     <S.SafeContainer>
-      <S.Header>
-        <S.BackButton onPress={() => navigation.goBack()}>
-          <S.BackIcon>←</S.BackIcon>
-        </S.BackButton>
-        <S.Title>강남구 논현동</S.Title>
-        <S.ProfileContainer>
-          <S.ProfileImage source={{ uri: 'https://avatars.githubusercontent.com/u/65541546?v=4' }} />
-        </S.ProfileContainer>
+      <S.Header style={{ top: insets.top }}>
+        <S.HeaderGradient
+          colors={['rgba(255, 255, 255, 1)', 'rgba(255, 255, 255, 0)']}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 0, y: 1 }}
+        />
+        <S.HeaderContent>
+          <S.BackButton onPress={() => navigation.goBack()}>
+            <S.BackIcon>←</S.BackIcon>
+          </S.BackButton>
+          <S.Title fontSize={20}>강남구 논현동</S.Title>
+          <S.ProfileContainer>
+            <S.ProfileImage source={{ uri: 'https://avatars.githubusercontent.com/u/65541546?v=4' }} />
+          </S.ProfileContainer>
+        </S.HeaderContent>
       </S.Header>
 
       <S.MapContainer>
--- src/screens/Home/walk-styles.ts
+++ src/screens/Home/walk-styles.ts
@@ -1,4 +1,6 @@
 import styled from '@emotion/native';
+import LinearGradient from 'react-native-linear-gradient';
+import { TextBold } from '~components/Common/Text';
 
 export const SafeContainer = styled.SafeAreaView`
   flex: 1;
@@ -6,28 +8,44 @@ export const SafeContainer = styled.SafeAreaView`
 `;
 
 export const Header = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 16px;
-  background-color: white;
+  position: absolute;
+  left: 0;
+  right: 0;
+  z-index: 1;
   height: 80px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  height: 100%;
+`;
+export const HeaderGradient = styled(LinearGradient)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
 `;
 
 export const BackButton = styled.TouchableOpacity`
   padding: 8px;
+  width: 48px;
+  height: 48px;
+  background-color: ${({ theme }) => theme.colors.gc_2};
+  border-radius: 100px;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const BackIcon = styled.Text`
   font-size: 24px;
+  color: black;
 `;
 
-export const Title = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  flex: 1;
-  text-align: center;
-`;
+export const Title = styled(TextBold)``;
 
 export const ProfileContainer = styled.View`
   width: 40px;
@@ -37,7 +55,7 @@ export const ProfileContainer = styled.View`
 export const ProfileImage = styled.Image`
   width: 40px;
   height: 40px;
-  border-radius: 20px;
+  border-radius: 100px;
 `;
 
 export const MapContainer = styled.View`
