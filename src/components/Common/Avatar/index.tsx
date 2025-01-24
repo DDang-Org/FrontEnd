5807d0d6500bb7b72358b48991cda4fbf63fa3f4
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-24 15:31:52 +0900

    feat: Avatar

---
+++ src/components/Common/Avatar/index.tsx
@@ -0,0 +1,12 @@
+import Avatar1 from '~assets/avatars/Avatar1.svg';
+import Avatar2 from '~assets/avatars/Avatar2.svg';
+import Avatar3 from '~assets/avatars/Avatar3.svg';
+import Avatar4 from '~assets/avatars/Avatar4.svg';
+import Avatar5 from '~assets/avatars/Avatar5.svg';
+import Avatar6 from '~assets/avatars/Avatar6.svg';
+import Avatar7 from '~assets/avatars/Avatar7.svg';
+import Avatar8 from '~assets/avatars/Avatar8.svg';
+import Avatar9 from '~assets/avatars/Avatar9.svg';
+import Avatar10 from '~assets/avatars/Avatar10.svg';
+
+export const Avatar = [null, Avatar1, Avatar2, Avatar3, Avatar4, Avatar5, Avatar6, Avatar7, Avatar8, Avatar9, Avatar10];
