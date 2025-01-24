af7ac21d2cdc833475aa261e2eaacae3e699b4d3
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-01-24 22:56:17 +0900

    feat: Profile 컴포넌트 개선
    Profile.Mine 추가

--- src/components/Common/Profile/index.tsx
+++ src/components/Common/Profile/index.tsx
@@ -1,14 +1,18 @@
 import React, { Suspense } from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { SvgProps } from 'react-native-svg';
+import { useUser } from '~apis/member/useUser';
+import { AvatarNumber } from '~types/avatar-number';
 import { getAvatar } from '~utils/getAvatar';
 import * as S from './styles';
+import ErrorBoundary, { FallbackComponentProps } from 'react-native-error-boundary';
+
 interface ProfileProps {
   size: number;
   src?: string | React.FC<SvgProps>;
   userId?: number;
   testID?: string;
-  avatarNumber?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+  avatarNumber?: AvatarNumber;
 }
 
 export const Profile = ({ size, src, userId, testID, avatarNumber }: ProfileProps) => {
@@ -29,7 +33,7 @@ export const Profile = ({ size, src, userId, testID, avatarNumber }: ProfileProp
       throw new Error('Profile 컴포넌트의 props가 적절하지 않습니다. src, avatarNumber 중 하나는 작성해 주세요.');
     }
     if (typeof src === 'string') {
-      return <Image source={{ uri: src }} style={{ width: size, height: size }} />;
+      return <S.ProfileWithSrc source={{ uri: src }} style={{ width: size, height: size }} />;
     }
 
     const SvgComponent = src;
@@ -58,3 +62,24 @@ export const Profile = ({ size, src, userId, testID, avatarNumber }: ProfileProp
 //   src={Avatar1}
 // userId 없음 - 터치 피드백 없이 항상 opacity 1 유지
 // />
+
+const DefaultProfile = ({ size }: { size: number }) => <S.ProfileImageLoader style={{ width: size, height: size }} />;
+
+const MyProfileFallback = ({}: FallbackComponentProps) => <S.ProfileImageFallback />;
+
+const MyProfileDataProvider = ({ size }: { size: number }) => {
+  const { avatarNumber, memberId } = useUser();
+  return <Profile size={size} avatarNumber={avatarNumber} userId={memberId} />;
+};
+
+const MyProfile = ({ size }: { size: number }) => {
+  return (
+    <ErrorBoundary FallbackComponent={MyProfileFallback}>
+      <Suspense fallback={<DefaultProfile size={size} />}>
+        <MyProfileDataProvider size={size} />
+      </Suspense>
+    </ErrorBoundary>
+  );
+};
+
+Profile.Mine = MyProfile;
