490b025260620f328e66167d9ec34adb33e5f7e7
kimjuyoung99 <jjassb404@naver.com>
2025-01-28 05:44:50 +0900

    fix : LoginComment 컬러 변경

--- src/components/Login/LoginComment/indedx.tsx
+++ src/components/Login/LoginComment/indedx.tsx
@@ -1,11 +1,13 @@
 import * as S from './styles';
+import { useTheme } from '@emotion/react';
 
 export const LoginComment = () => {
+  const theme = useTheme();
   return (
     <S.LoginComment>
       <S.LoginCommentText fontSize={28}>건강한 반려생활,</S.LoginCommentText>
       <S.LoginCommentText fontSize={28}>
-        <S.LoginCommentText fontSize={28} style={{ color: '#FFED16' }}>
+        <S.LoginCommentText fontSize={28} style={{ color: theme.colors.default }}>
           댕
         </S.LoginCommentText>
         과 함께해요!
