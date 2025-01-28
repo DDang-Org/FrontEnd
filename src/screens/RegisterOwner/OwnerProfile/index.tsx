3cccc6a2dd38189bdefb9874ad801c00af2ef58b
kimjuyoung99 <jjassb404@naver.com>
2025-01-28 09:43:03 +0900

    feat : 회원가입 데이터 입력 Toast 조건부 랜더링 설정

--- src/screens/RegisterOwner/OwnerProfile/index.tsx
+++ src/screens/RegisterOwner/OwnerProfile/index.tsx
@@ -4,13 +4,11 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { ActionButton } from '~components/Common/ActionButton';
 import { RootStackParamList } from '~navigation/RootNavigator';
 import { RegisterDogNavigations } from '~constants/navigations';
-// import { Icon } from '~components/Common/Icons';
 import { useState } from 'react';
 import { ScrollView } from 'react-native';
-
+import { Gender } from '~types/gender';
 import { BaseInput } from '~components/Common/BaseInput';
-// import { FormErrorToast } from '~components/Common/FormErrorToast';
-// import { FormErrorText } from '~components/Common/FormErrorText';
+import { FormErrorToast } from '~components/Common/FormErrorToast';
 import { GenderSelectButton } from '~components/Common/GenderSelectButton';
 import { PressableInput } from '~components/Common/PressableInput';
 
@@ -18,10 +16,24 @@ import { PressableInput } from '~components/Common/PressableInput';
 type RootNavigationProp = NativeStackNavigationProp<RootStackParamList, 'RegisterDog'>;
 
 export function RegisterOwnerProfile() {
-  // useNavigation을 사용해 최상위 네비게이터와 연결
   const navigation = useNavigation<RootNavigationProp>();
+  const [name, setName] = useState('');
   const [address, setAddress] = useState('');
   const [birth, setBirth] = useState('');
+  const [selectedGender, setSelectedGender] = useState<Gender | null>(null);
+  const isComplete = name && address && birth && selectedGender;
+  const [showToast, setShowToast] = useState(false);
+
+  const handleNextPress = () => {
+    if (!isComplete) {
+      setShowToast(true); // 필드가 완성되지 않은 경우 Toast 표시
+    } else {
+      setShowToast(false); // Toast 숨김
+      navigation.navigate('RegisterDog', {
+        screen: RegisterDogNavigations.HOME,
+      });
+    }
+  };
 
   return (
     <ScrollView contentContainerStyle={{ paddingBottom: 100, padding: 16 }}>
@@ -29,48 +41,40 @@ export function RegisterOwnerProfile() {
         <S.LoginCommentText fontSize={24}>견주님에 대해</S.LoginCommentText>
         <S.LoginCommentText fontSize={24}>알려주세요</S.LoginCommentText>
       </S.LoginComment>
-
-      {/* <S.RegisterOwner> */}
       <S.AvatarSelectWrapper>
         <S.StyledAvatarSelect onPress={() => navigation.navigate('RegisterOwner', { screen: 'OwnerAvatarModal' })} />
 
         <S.ProfileDataContainer>
-          <BaseInput placeholder="닉네임 입력" />
-          <PressableInput onPress={() => navigation.navigate('RegisterOwner', {screen:'OwnerAvatarModal'})} value={birth} placeholder="가족 포지션 입력" />
+          <BaseInput placeholder="닉네임 입력" value={name} onChangeText={setName} />
+          <PressableInput
+            onPress={() => navigation.navigate('RegisterOwner', { screen: 'OwnerAvatarModal' })}
+            value={address}
+            placeholder="가족 포지션 입력"
+          />
           <PressableInput onPress={() => setAddress('양천구 신월동')} value={address} placeholder="내 동네 불러오기" />
-
           <PressableInput onPress={() => setBirth('2000.09.23')} value={birth} placeholder="생년월일 선택" />
 
           <S.GenderButtonWrapper>
             <GenderSelectButton
               gender="MALE"
-              isActive={false}
+              isActive={selectedGender === 'MALE'}
               direction="row"
-              onPress={() => console.log('남자 클릭')}
+              onPress={() => setSelectedGender(selectedGender === 'MALE' ? null : 'MALE')}
             />
             <GenderSelectButton
               gender="FEMALE"
-              isActive={false}
+              isActive={selectedGender === 'FEMALE'}
               direction="row"
-              onPress={() => console.log('여자 클릭')}
+              onPress={() => setSelectedGender(selectedGender === 'FEMALE' ? null : 'FEMALE')}
             />
           </S.GenderButtonWrapper>
         </S.ProfileDataContainer>
       </S.AvatarSelectWrapper>
-
+      {showToast && <FormErrorToast message="모든 정보를 입력해 주세요" />}a
       {/* 버튼 클릭 시 최상위 네비게이터의 'RegisterDog' 경로로 이동 */}
       <S.NextButtonWrapper>
-        <ActionButton
-          onPress={
-            () =>
-              navigation.navigate('RegisterDog', {
-                screen: RegisterDogNavigations.HOME,
-              }) //중첩 네비게이터 호출 방식
-          }
-          text="다음"
-        />
+        <ActionButton onPress={handleNextPress} text="다음" disabled={!isComplete} />
       </S.NextButtonWrapper>
-      {/* </S.RegisterOwner> */}
     </ScrollView>
   );
 }
--- src/screens/RegisterOwner/OwnerProfile/styles.ts
+++ src/screens/RegisterOwner/OwnerProfile/styles.ts
@@ -1,36 +1,30 @@
-import styled from "@emotion/native";
-import { TextBold } from "~components/Common/Text";
-import { Icon } from "~components/Common/Icons";
+import styled from '@emotion/native';
+import { TextBold } from '~components/Common/Text';
+import { Icon } from '~components/Common/Icons';
 
 export const LoginComment = styled.View`
-  margin-top: 40px;
-  margin-bottom: 30px;
-  align-items: center;
+    margin-top: 40px;
+    margin-bottom: 30px;
+    align-items: center;
 `;
 
 export const LoginCommentText = styled(TextBold)`
-  text-align: center;
-`;
-
-export const RegisterOwner = styled.View`
-    padding: 10px;
-    background-color: ${({theme})=> theme.colors.gc_4};
+    text-align: center;
 `;
 
 export const AvatarSelectWrapper = styled.View`
     align-items: center;
 `;
 
-export const ProfileDataContainer = styled.View`
- margin: 10px;
- gap: 4px;
- align-items: center;
+export const StyledAvatarSelect = styled(Icon.AvatarSelect)`
+    margin-top:10px;
+    margin-bottom: 20px;
 `;
 
-
-export const StyledAvatarSelect = styled(Icon.AvatarSelect)`
-   margin-top:10px;
-   margin-bottom: 20px;
+export const ProfileDataContainer = styled.View`
+    margin: 10px;
+    gap: 4px;
+    align-items: center;
 `;
 
 export const GenderButtonWrapper = styled.View`
@@ -40,9 +34,9 @@ export const GenderButtonWrapper = styled.View`
 `;
 
 export const NextButtonWrapper = styled.View`
-position: absolute;
-bottom: 24px;
-left:0;
-right:0;
-padding:16px;
+    position: absolute;
+    bottom: 24px;
+    left:0;
+    right:0;
+    padding:16px;
 `;
