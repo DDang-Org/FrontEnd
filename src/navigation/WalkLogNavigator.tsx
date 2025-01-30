86110d739ad9dfc6017f4196a0c2fa6b5b7055c8
wonil <nwi6959@gmail.com>
2025-01-30 12:12:51 +0900

    feat: 산책 로그 탭 헤더 추가

---
+++ src/navigation/WalkLogNavigator.tsx
@@ -0,0 +1,57 @@
+import { useTheme } from '@emotion/react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Pressable } from 'react-native';
+import { Icon } from '~components/Common/Icons';
+import { DogProfile } from '~components/Log/DogProfile';
+import { WalkLogNavigations } from '~constants/navigations';
+import { LogHome } from '~screens/Log';
+import { Stats } from '~screens/Log/Stats';
+
+export type WalkLogParamList = {
+  LogHome: undefined;
+  Stats: undefined;
+};
+
+export const WalkLogNavigator = () => {
+  const Stack = createNativeStackNavigator<WalkLogParamList>();
+  const theme = useTheme();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShadowVisible: false,
+        animation: 'slide_from_right',
+        presentation: 'card',
+      }}
+    >
+      <Stack.Screen
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <DogProfile dogName="밤톨이" imageUri="https://dimg.donga.com/wps/NEWS/IMAGE/2022/01/28/111500268.2.jpg" />
+          ),
+          headerTitle: '',
+          headerRight: () => (
+            <Pressable onPress={() => navigation.navigate(WalkLogNavigations.Stats)}>
+              <Icon.Graph />
+            </Pressable>
+          ),
+        })}
+        name={WalkLogNavigations.LogHome}
+        component={LogHome}
+      />
+      <Stack.Screen
+        options={({ navigation }) => ({
+          headerStyle: {
+            backgroundColor: theme.colors.lighten_3,
+          },
+          headerLeft: () => (
+            <Pressable onPress={() => navigation.goBack()}>
+              <Icon.Prev />
+            </Pressable>
+          ),
+        })}
+        name={WalkLogNavigations.Stats}
+        component={Stats}
+      />
+    </Stack.Navigator>
+  );
+};
---
+++ src/screens/Log/Stats/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './styles';
+
+interface StatsProps {}
+
+export const Stats = ({}: StatsProps) => {
+  return <S.Stats></S.Stats>;
+};
