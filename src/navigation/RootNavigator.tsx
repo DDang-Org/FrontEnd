ea0e8e6c571b4c27f04f232dadb617e7d0a70564
wonil <nwi6959@gmail.com>
2025-01-31 15:16:41 +0900

    fix : 캘린더 오류 수정

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,7 +1,33 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RegisterOwnerParamList } from '~navigation/Auth/RegisterOwnerNavigator';
 import { BottomTabNavigator } from '~navigation/BottomTabNavigator';
-// import { RegisterDogNavigator } from '~navigation/RegisterDogNavigator';
+import { RegisterDogParamList } from '~navigation/RegisterDogNavigator';
+import { LoginParamList } from '~navigation/Auth/LoginNavigator';
+
+export type RootStackParamList = {
+  Login: { screen?: keyof LoginParamList };
+  RegisterOwner: { screen?: keyof RegisterOwnerParamList };
+  RegisterDog: { screen?: keyof RegisterDogParamList };
+  BottomTab: undefined;
+};
 
 export const RootNavigator = () => {
-  return <BottomTabNavigator />;
-  // return <RegisterDogNavigator />;
+  const Stack = createNativeStackNavigator<RootStackParamList>();
+
+  // 현재 RootNavigator에서 조건부 렌더링(if)을 사용하지 않고 모든 네비게이터를 한 번에 등록하도록 설정
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {/* <Stack.Screen
+        name="RegisterOwner"
+        component={RegisterOwnerNavigator}
+        options={{ headerShown: true, headerTitle: '' }}
+      /> */}
+      {/* <Stack.Screen name="RegisterDog" component={RegisterDogNavigator} /> */}
+      {/* <Stack.Screen name="BottomTab" component={BottomTabNavigator} /> */}
+
+      {/* <Stack.Screen name="Login" component={LoginNavigator} options={{ headerTitle: '' }} /> */}
+
+      <Stack.Screen name="BottomTab" component={BottomTabNavigator} />
+    </Stack.Navigator>
+  );
 };
