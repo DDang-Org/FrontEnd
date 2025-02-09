a2a281e9654d15f5c236d28aaa9c8e347387ba49
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-02-09 19:35:46 +0900

    feat: Friend 백엔드 연동

--- src/components/Common/UserInfo/UserInfo.stories.tsx
+++ src/components/Common/UserInfo/UserInfo.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta } from '@storybook/react';
 import { UserInfo } from './index';
+import { Gender } from '~types/gender';
+import { AvatarNumber } from '~types/avatar-number';
+import { FamilyRole } from '~types/family-role';
 
 const meta = {
   title: 'UserInfo',
@@ -22,27 +25,36 @@ export default meta;
 
 export const MultipleItemsExample = {
   render: () => (
-    <UserInfo.Container
-      data={Array.from({ length: 20 }, (_, index) => ({
+    <UserInfo.Container>
+      {Array.from({ length: 20 }, (_, index) => ({
         name: `User ${index + 1}`,
-        gender: index % 2 === 0 ? 'MALE' : 'FEMALE',
-        dogGender: index % 3 === 0 ? 'FEMALE' : 'MALE',
-        familyRole: index % 4 === 0 ? 'BROTHER' : index % 4 === 1 ? 'SISTER' : index % 4 === 2 ? 'MOTHER' : 'FATHER',
+        gender: index % 2 === 0 ? 'MALE' : ('FEMALE' as Gender),
+        dogGender: index % 3 === 0 ? 'FEMALE' : ('MALE' as Gender),
+        familyRole: (index % 4 === 0
+          ? 'BROTHER'
+          : index % 4 === 1
+          ? 'SISTER'
+          : index % 4 === 2
+          ? 'MOTHER'
+          : 'FATHER') as FamilyRole,
         buttonText: index % 2 === 0 ? 'Edit' : 'View',
         onPressButton: () => {},
-      }))}
-      keyExtractor={(item, index) => `${item.name}-${index}`}
-      renderItem={({ item, index }) => (
+        avatarNumber: 1 as AvatarNumber,
+        userId: index,
+      })).map((item, index) => (
         <UserInfo.Item
           name={item.name}
+          key={`${item.name}-${index}`}
           gender={item.gender}
           dogGender={item.dogGender}
           familyRole={item.familyRole}
           buttonText={item.buttonText}
           isLast={index === 19} // Mark the last item
           onPressButton={() => console.log(`${item.name} button clicked`)}
+          avatarNumber={item.avatarNumber}
+          userId={item.userId}
         />
-      )}
-    />
+      ))}
+    </UserInfo.Container>
   ),
 };
--- src/components/Social/Friend/index.tsx
+++ src/components/Social/Friend/index.tsx
@@ -1,34 +1,52 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { useBlockedUsers } from '~apis/member/useBlockedUsers';
+import { useDogInfoByMemberId } from '~apis/dog/useDogInfoByMemberId';
+import { FetchFriendsResponseType } from '~apis/friend/fetchFriends';
+import { useFriends } from '~apis/friend/useFriends';
 import { UserInfo } from '~components/Common/UserInfo';
 import { TabBarParamList } from '~navigation/BottomTabNavigator';
 
 export const FriendTab = () => {
-  const navigation = useNavigation<NavigationProp<TabBarParamList>>();
-  const blockedUsers = useBlockedUsers();
-
+  const friends = useFriends();
+  console.log(friends);
   return (
     <UserInfo>
       (
-      <UserInfo.Container
-        data={blockedUsers?.map(user => ({
-          ...user,
-          userId: user.memberId,
-          onPressButton: () => {},
-          buttonText: '메시지',
-          isLast: false,
-        }))}
-        keyExtractor={(item, index) => `${item.name}-${index}`}
-        renderItem={({ item, index }) => (
-          <UserInfo.Item
-            {...item}
-            isLast={index === blockedUsers.length - 1}
-            // 채팅창 띄우기
-            onPressButton={() => navigation.navigate('Talk', { userId: item.userId })}
-          />
-        )}
-      />
+      <UserInfo.Container>
+        {friends?.map((friend, idx) => (
+          <Item key={friend.memberId} friend={friend} memberId={friend.memberId} isLast={idx === friends.length - 1} />
+        ))}
+      </UserInfo.Container>
       ),
     </UserInfo>
+  );
+};
+
+const Item = ({
+  friend,
+  memberId,
+  isLast,
+}: {
+  friend: FetchFriendsResponseType[number];
+  memberId: number;
+  isLast: boolean;
+}) => {
+  const dogInfos = useDogInfoByMemberId({ memberId });
+  const navigation = useNavigation<NavigationProp<TabBarParamList>>();
+  console.log({ dogInfos }); //todo 3번 멤버 강아지 없어서 에러 발생. 데이터 추가 요청하기
+  if (!dogInfos || !dogInfos.length) {
+    return null;
+  }
+  return (
+    <UserInfo.Item
+      avatarNumber={friend.memberProfileImg}
+      buttonText="메시지"
+      dogGender={dogInfos[0].dogGender}
+      familyRole={friend.familyRole}
+      gender={friend.memberGender}
+      name={friend.memberName}
+      onPressButton={() => navigation.navigate('Talk', { userId: friend.memberId })}
+      userId={friend.memberId}
+      isLast={isLast}
+    />
   );
 };
