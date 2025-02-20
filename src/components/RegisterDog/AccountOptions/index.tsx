30daf1c3093d2e38c20b28bba6d8568ae8210827
wonil <nwi6959@gmail.com>
2025-02-20 20:35:42 +0900

    feat: 강아지 등록 홈화면 회원탈퇴 기능 추가

--- src/components/RegisterDog/AccountOptions/index.tsx
+++ src/components/RegisterDog/AccountOptions/index.tsx
@@ -1,3 +1,4 @@
+import { Alert } from 'react-native';
 import { useAuth } from '~apis/member/useAuth';
 import { CompoundOption } from '~components/Common/CompoundOptions';
 
@@ -7,14 +8,27 @@ interface AccountOptionsProps {
 }
 
 export const AccountOptions = ({ isVisible, hideOption }: AccountOptionsProps) => {
-  const { logoutMutation } = useAuth();
+  const { logoutMutation, deleteAccountMutation } = useAuth();
+
+  const handleDeleteAccount = () => {
+    Alert.alert('정말로 탈퇴하시겠습니까?', '삭제된 계정은 복구하실 수 없습니다.', [
+      {
+        text: '탈퇴하기',
+        onPress: () => deleteAccountMutation.mutate(null),
+      },
+      {
+        text: '취소',
+        style: 'cancel',
+      },
+    ]);
+  };
   return (
     <CompoundOption isVisible={isVisible} hideOption={hideOption}>
       <CompoundOption.Background>
         <CompoundOption.Container>
           <CompoundOption.Button onPress={() => logoutMutation.mutate(null)}>로그아웃</CompoundOption.Button>
           <CompoundOption.Divider />
-          <CompoundOption.Button>회원 탈퇴</CompoundOption.Button>
+          <CompoundOption.Button onPress={handleDeleteAccount}>회원 탈퇴</CompoundOption.Button>
         </CompoundOption.Container>
         <CompoundOption.Container>
           <CompoundOption.Button isDanger onPress={hideOption}>
