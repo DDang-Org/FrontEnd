e375293ee1e7fefdce604b01a7db1b19f202ded3
kimjuyoung99 <jjassb404@naver.com>
2025-02-21 02:44:21 +0900

    feat : familyCount와 userInfo.isRepresentative 조건에 따라 버튼을 동적으로 렌더링 (3가지 경우의 수).

--- src/screens/FamilyDang/FamilySetting/index.tsx
+++ src/screens/FamilyDang/FamilySetting/index.tsx
@@ -1,27 +1,30 @@
-import { TextBold, TextRegular } from '~components/Common/Text';
 import * as S from './styles';
 import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
+import { TextBold, TextRegular } from '~components/Common/Text';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { FamilyDdangParamList } from '~navigation/FamilyDDangNavigator';
+import { FamilyDdangParamList } from '~navigation/FamilyDdangNavigator';
 import { RootStackParamList } from '~navigation/RootNavigator';
 import { RegisterDogParamList } from '~navigation/RegisterDogNavigator';
 import { RegisterDogNavigations } from '~constants/navigations';
 import { useUser } from '~apis/member/useUser';
+import { useFamilyInfo } from '~apis/family/useFamilyInfo';
 
 type NavigationProp = NativeStackNavigationProp<FamilyDdangParamList>;
 type CompositeNavigationType = CompositeNavigationProp<
   NativeStackNavigationProp<RootStackParamList>,
   NativeStackNavigationProp<RegisterDogParamList>
 >;
-// type RegisterDogProps = NativeStackNavigationProp<RegisterDogParamList>;
 
 export const FamilySetting = () => {
   const userInfo = useUser();
-  console.log('패밀리장??', userInfo.isRepresentative);
-  console.log('패밀리장??', userInfo.address);
+  const familyResponse = useFamilyInfo();
+  const familyMembers = familyResponse || [];
+  const familyCount = familyMembers.length;
+
+  console.log('패밀리 구성원 수:', familyCount);
+
   const navigation = useNavigation<NavigationProp>();
   const compositeNavigation = useNavigation<CompositeNavigationType>();
-  // const navigationDog = useNavigation<RegisterDogProps>();
 
   //! 1. (1,2)강아지 추가하기
   const handleRegisterDog = () => {
@@ -29,22 +32,25 @@ export const FamilySetting = () => {
       screen: RegisterDogNavigations.BASIC_PROFILE,
     });
   };
+
   //! 2. (1)패밀리 퇴출하기
   const handleFamilyOut = () => {
     navigation.navigate('FamilyOut');
   };
+
   //! 3. (1,2,3)패밀리 스스로 나가기
   const handleFamilyOutMySelf = () => {
     if (userInfo.isRepresentative === true) {
-      //패밀리장일 경우 양도하기 패이지로 이동
+      // 패밀리장일 경우 양도하기 페이지로 이동
       navigation.navigate('FamilyCaptain');
     } else {
-      //패밀리 구성원일 경우 바로 나가지고 강아지 등록 페이지로 이동
+      // 패밀리 구성원일 경우 바로 나가고 강아지 등록 페이지로 이동
       compositeNavigation.navigate('RegisterDog', {
         screen: RegisterDogNavigations.HOME,
       });
     }
   };
+
   //! 4. (2)패밀리 가입하기
   const handleJoinFamily = () => {
     compositeNavigation.navigate('RegisterDog', {
@@ -55,34 +61,61 @@ export const FamilySetting = () => {
   return (
     <S.FamilySetting>
       <S.FamilystyledView>
-        <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
-          <S.TypoWrapper>
-            <TextBold fontSize={17}>강아지 추가하기</TextBold>
-            <TextRegular fontSize={15}>패밀리장은 강아지를 추가할 수 있어요</TextRegular>
-          </S.TypoWrapper>
-          <S.NextButton onPress={handleRegisterDog} />
-        </S.NavigationToSettingScreen>
-        <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
-          <S.TypoWrapper>
-            <TextBold fontSize={17}>패밀리 나가기</TextBold>
-            <TextRegular fontSize={15}>강아지를 등록하거나 다른 패밀리에 들어가야 해요</TextRegular>
-          </S.TypoWrapper>
-          <S.NextButton onPress={handleFamilyOutMySelf} />
-        </S.NavigationToSettingScreen>
-        <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
-          <S.TypoWrapper>
-            <TextBold fontSize={17}>패밀리 퇴출하기</TextBold>
-            <TextRegular fontSize={15}>패밀리장은 패밀리 구성원을 퇴출시킬 수 있어요</TextRegular>
-          </S.TypoWrapper>
-          <S.NextButton onPress={handleFamilyOut} />
-        </S.NavigationToSettingScreen>
-        <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
-          <S.TypoWrapper>
-            <TextBold fontSize={17}>패밀리 들어가기</TextBold>
-            <TextRegular fontSize={15}>가족 코드를 입력해 다른 패밀리에 들어갈 수 있어요</TextRegular>
-          </S.TypoWrapper>
-          <S.NextButton onPress={handleJoinFamily} />
-        </S.NavigationToSettingScreen>
+        {/* 조건에 따라 NavigationToSettingScreen 렌더링 */}
+        {familyCount >= 2 && userInfo.isRepresentative === true && (
+          <>
+            <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
+              <S.TypoWrapper>
+                <TextBold fontSize={17}>강아지 추가하기</TextBold>
+                <TextRegular fontSize={15}>패밀리장은 강아지를 추가할 수 있어요</TextRegular>
+              </S.TypoWrapper>
+              <S.NextButton onPress={handleRegisterDog} />
+            </S.NavigationToSettingScreen>
+            <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
+              <S.TypoWrapper>
+                <TextBold fontSize={17}>패밀리 나가기</TextBold>
+                <TextRegular fontSize={15}>강아지를 등록하거나 다른 패밀리에 들어가야 해요</TextRegular>
+              </S.TypoWrapper>
+              <S.NextButton onPress={handleFamilyOutMySelf} />
+            </S.NavigationToSettingScreen>
+            <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
+              <S.TypoWrapper>
+                <TextBold fontSize={17}>패밀리 퇴출하기</TextBold>
+                <TextRegular fontSize={15}>패밀리장은 패밀리 구성원을 퇴출시킬 수 있어요</TextRegular>
+              </S.TypoWrapper>
+              <S.NextButton onPress={handleFamilyOut} />
+            </S.NavigationToSettingScreen>
+          </>
+        )}
+        {familyCount >= 2 && userInfo.isRepresentative === false && (
+          <>
+            <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
+              <S.TypoWrapper>
+                <TextBold fontSize={17}>패밀리 나가기</TextBold>
+                <TextRegular fontSize={15}>강아지를 등록하거나 다른 패밀리에 들어가야 해요</TextRegular>
+              </S.TypoWrapper>
+              <S.NextButton onPress={handleFamilyOutMySelf} />
+            </S.NavigationToSettingScreen>
+          </>
+        )}
+        {familyCount === 1 && (
+          <>
+            <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
+              <S.TypoWrapper>
+                <TextBold fontSize={17}>강아지 추가하기</TextBold>
+                <TextRegular fontSize={15}>패밀리장은 강아지를 추가할 수 있어요</TextRegular>
+              </S.TypoWrapper>
+              <S.NextButton onPress={handleRegisterDog} />
+            </S.NavigationToSettingScreen>
+            <S.NavigationToSettingScreen paddingHorizontal={20} paddingVertical={16.5}>
+              <S.TypoWrapper>
+                <TextBold fontSize={17}>패밀리 들어가기</TextBold>
+                <TextRegular fontSize={15}>가족 코드를 입력해 다른 패밀리에 들어갈 수 있어요</TextRegular>
+              </S.TypoWrapper>
+              <S.NextButton onPress={handleJoinFamily} />
+            </S.NavigationToSettingScreen>
+          </>
+        )}
       </S.FamilystyledView>
     </S.FamilySetting>
   );
