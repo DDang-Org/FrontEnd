e5eed8840fd2820000158d1c821ae6a23748492c
kimjuyoung99 <jjassb404@naver.com>
2025-02-20 21:44:37 +0900

    feat : 패밀리장 위임하기 단일 선택 기능 추가, 주석 삭제

--- src/screens/FamilyDang/FamilySetting/FamilyCaptain/index.tsx
+++ src/screens/FamilyDang/FamilySetting/FamilyCaptain/index.tsx
@@ -3,12 +3,14 @@ import * as S from '../styles';
 import { ClickFamily } from '~screens/FamilyDang/FamilyInfo/clickfamily';
 import { FamilyComment } from '~screens/FamilyDang/FamilyInfo/familycomment';
 import { ActionButton } from '~components/Common/ActionButton';
+import { useFamilyInfo } from '~apis/family/useFamilyInfo';
 
 export const FamilyCaptain = () => {
-  const [isAnySelected, setIsAnySelected] = useState(false);
+  const familyMembers = useFamilyInfo();
+  const [selectedMemeberId, setSelectedMemberId] = useState<number | null>(null);
 
-  const handleSelect = (isSelected: boolean) => {
-    setIsAnySelected(isSelected);
+  const handleSelect = (memberId: number) => {
+    setSelectedMemberId(memberId);
   };
 
   return (
@@ -18,12 +20,21 @@ export const FamilyCaptain = () => {
           title="패밀리장을 위임할 사람을 선택하세요"
           description="패밀리장은 패밀리를 나갈 때 권한을 위임해야 해요"
         />
-        {[...Array(5)].map((_, index) => (
-          <ClickFamily key={index} onSelect={handleSelect} />
+        {familyMembers?.map(member => (
+          <ClickFamily
+            key={member.memberId}
+            memberId={member.memberId}
+            memberName={member.memberName}
+            isSelected={selectedMemeberId === member.memberId}
+            onSelect={handleSelect}
+          />
         ))}
-
         <S.ActionButtonWrapper>
-          <ActionButton onPress={() => console.log('Action 버튼 클릭')} text="위임하기" disabled={!isAnySelected} />
+          <ActionButton
+            onPress={() => console.log('Action 버튼 클릭')}
+            text="패밀리장 위임하기"
+            disabled={selectedMemeberId === null}
+          />
         </S.ActionButtonWrapper>
       </S.StyledView>
     </S.FamilySetting>
