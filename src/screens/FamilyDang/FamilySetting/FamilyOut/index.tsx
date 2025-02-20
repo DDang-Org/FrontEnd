5300638644173e052cc2fd13f077662afd253dfa
kimjuyoung99 <jjassb404@naver.com>
2025-02-20 21:46:04 +0900

    feat : 패밀리장 위임, 퇴출 회원 자신 제외

--- src/screens/FamilyDang/FamilySetting/FamilyOut/index.tsx
+++ src/screens/FamilyDang/FamilySetting/FamilyOut/index.tsx
@@ -20,7 +20,7 @@ export const FamilyOut = () => {
           title="패밀리를 퇴출시킬 구성원을 선택하세요"
           description="패밀리장은 악성유저 등 패밀리 구성원을 퇴출시킬 수 있어요"
         />
-        {familyMembers?.map(member => (
+        {familyMembers?.slice(1).map(member => (
           <ClickFamily
             key={member.memberId}
             memberId={member.memberId}
