c1aa4baf770964afec4e6b89d75079977d3140e1
kimjuyoung99 <jjassb404@naver.com>
2025-02-20 21:40:41 +0900

    feat : 패밀리 퇴출하기 단일 선택 기능 추가. ClickFamily컴포넌트 로직 수정

--- src/screens/FamilyDang/FamilySetting/FamilyOut/index.tsx
+++ src/screens/FamilyDang/FamilySetting/FamilyOut/index.tsx
@@ -3,12 +3,15 @@ import * as S from '../styles';
 import { ClickFamily } from '~screens/FamilyDang/FamilyInfo/clickfamily';
 import { FamilyComment } from '~screens/FamilyDang/FamilyInfo/familycomment';
 import { ActionButton } from '~components/Common/ActionButton';
+import { useFamilyInfo } from '~apis/family/useFamilyInfo';
 
 export const FamilyOut = () => {
-  const [isAnySelected, setIsAnySelected] = useState(false); // 하나라도 선택되었는지 확인
+  // const [isAnySelected, setIsAnySelected] = useState(false); // 하나라도 선택되었는지 확인
+  const familyMembers = useFamilyInfo();
+  const [selectedMemeberId, setSelectedMemberId] = useState<number | null>(null);
 
-  const handleSelect = (isSelected: boolean) => {
-    setIsAnySelected(isSelected);
+  const handleSelect = (memberId: number) => {
+    setSelectedMemberId(memberId);
   };
 
   return (
@@ -18,11 +21,21 @@ export const FamilyOut = () => {
           title="패밀리를 퇴출시킬 구성원을 선택하세요"
           description="패밀리장은 악성유저 등 패밀리 구성원을 퇴출시킬 수 있어요"
         />
-        {[...Array(4)].map((_, index) => (
-          <ClickFamily key={index} onSelect={handleSelect} />
+        {familyMembers?.map(member => (
+          <ClickFamily
+            key={member.memberId}
+            memberId={member.memberId}
+            memberName={member.memberName}
+            isSelected={selectedMemeberId === member.memberId}
+            onSelect={handleSelect}
+          />
         ))}
         <S.ActionButtonWrapper>
-          <ActionButton onPress={() => console.log('Action 버튼 클릭')} text="위임하기" disabled={!isAnySelected} />
+          <ActionButton
+            onPress={() => console.log('Action 버튼 클릭')}
+            text="퇴출하기"
+            disabled={selectedMemeberId === null}
+          />
         </S.ActionButtonWrapper>
       </S.StyledView>
     </S.FamilySetting>
