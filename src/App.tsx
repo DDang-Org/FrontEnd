daf2f13e2d61604a9a5cd18f07905e4f4f7bad88
wonil <nwi6959@gmail.com>
2025-02-23 06:13:38 +0900

    chore: 사용하지 않는 스토리북 관련 코드 주석처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import styled from '@emotion/native';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { useState } from 'react';
 import { AppProviders } from '~providers/AppProviders';
 import { lightTheme } from '~styles/theme';
 import { RootNavigator } from '~navigation/RootNavigator';
@@ -34,13 +32,13 @@ const MainApp = () => {
 
 export const App = () => {
   // const { isMswEnabled } = useInitializeMsw();
-  const [storybookEnabled, setStorybookEnabled] = useState(false);
+  // const [storybookEnabled, setStorybookEnabled] = useState(false);
 
   // if (__DEV__ && !isMswEnabled) {
   // return <Text>Loading MSW...</Text>;
   // }
 
-  const toggleStorybook = () => setStorybookEnabled(prev => !prev);
+  // const toggleStorybook = () => setStorybookEnabled(prev => !prev);
 
   return (
     <>
@@ -55,20 +53,20 @@ export const App = () => {
   );
 };
 
-const StoryBookFloatingButton = styled.TouchableOpacity`
-  position: absolute;
-  right: 30px;
-  bottom: 30px;
-  z-index: 1000;
-  width: 60px;
-  height: 60px;
-  border-radius: 30px;
-  background-color: purple;
-  align-items: center;
-  justify-content: center;
-`;
+// const StoryBookFloatingButton = styled.TouchableOpacity`
+//   position: absolute;
+//   right: 30px;
+//   bottom: 30px;
+//   z-index: 1000;
+//   width: 60px;
+//   height: 60px;
+//   border-radius: 30px;
+//   background-color: purple;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
-const StoryBookButtonText = styled.Text`
-  color: white;
-  font-size: 24px;
-`;
+// const StoryBookButtonText = styled.Text`
+//   color: white;
+//   font-size: 24px;
+// `;
