a127dae89ad33776ed2124b9cc5e1aefa3551900
kimjuyoung99 <jjassb404@naver.com>
2025-04-06 00:41:07 +0900

    feature : 이용약관 추가 및 Scroll View 스타일링

--- src/screens/MyPage/TermsOfUse/index.tsx
+++ src/screens/MyPage/TermsOfUse/index.tsx
@@ -1,31 +1,33 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { MyPageStackProps } from '~navigation/MyPageNavigator';
-import * as S from './styles';
+import React from 'react';
+// import { NativeStackScreenProps } from '@react-navigation/native-stack';
+// import { MyPageStackProps } from '~navigation/MyPageNavigator';
+import { ImageSourcePropType } from 'react-native'; // Import ImageSourcePropType
 import Term1 from '~assets/termsofuse/term1.png';
-import Term2 from '~assets/termsofuse/term1.png';
-import Term3 from '~assets/termsofuse/term1.png';
-import Term4 from '~assets/termsofuse/term1.png';
-import Term5 from '~assets/termsofuse/term1.png';
-import Term6 from '~assets/termsofuse/term1.png';
+import Term2 from '~assets/termsofuse/term2.png';
+import Term3 from '~assets/termsofuse/term3.png';
+import Term4 from '~assets/termsofuse/term4.png';
+import Term5 from '~assets/termsofuse/term5.png';
+import Term6 from '~assets/termsofuse/term6.png';
+import * as S from './styles';
 
-type Props = NativeStackScreenProps<MyPageStackProps, 'TermsOfUse'>;
+// type Props = NativeStackScreenProps<MyPageStackProps, 'TermsOfUse'>;
 
-export const TermsOfUseScreen = ({ navigation }: Props) => {
-  const handleBackPress = () => {
-    navigation.goBack();
-  };
+export const TermsOfUseScreen = () => {
+  // const handleBackPress = () => {
+  //   navigation.goBack();
+  // };
 
   return (
-    <S.SettingScreen>
-      <S.NavigationToBlockScreen>
-        <S.TypoWrapper>
-          <S.TermsOfUseTypo fontSize={15}>서비스 이용약관</S.TermsOfUseTypo>
-        </S.TypoWrapper>
-        <S.NextButton onPress={handleBackPress} />
-      </S.NavigationToBlockScreen>
-      <S.TermsOfUseButton onPress={handleBackPress}>
-        <Term1 />
-      </S.TermsOfUseButton>
-    </S.SettingScreen>
+    <S.Container>
+      <S.ScrollContainer>
+        {/* 각 이미지 렌더링 */}
+        <S.Image source={Term1 as ImageSourcePropType} resizeMode="contain" />
+        <S.Image source={Term2 as ImageSourcePropType} resizeMode="contain" />
+        <S.Image source={Term3 as ImageSourcePropType} resizeMode="contain" />
+        <S.Image source={Term4 as ImageSourcePropType} resizeMode="contain" />
+        <S.Image source={Term5 as ImageSourcePropType} resizeMode="contain" />
+        <S.Image source={Term6 as ImageSourcePropType} resizeMode="contain" />
+      </S.ScrollContainer>
+    </S.Container>
   );
 };
--- src/screens/MyPage/TermsOfUse/styles.ts
+++ src/screens/MyPage/TermsOfUse/styles.ts
@@ -1,45 +1,17 @@
 import styled from '@emotion/native';
-import { Pressable } from 'react-native';
-import { BgBox } from '~components/Common/BgBox';
-import { Icon } from '~components/Common/Icons';
-import { TextBold } from '~components/Common/Text';
 
-export const SettingScreen = styled.View`
+export const Container = styled.View`
   flex: 1;
-  padding: 26px 20px;
-  gap: 20px;
-  position: relative;
+  background-color: #ffffff;
 `;
 
-export const DeleteAccountButton = styled(Pressable)``;
-
-export const TermsOfUseButton = styled(Pressable)``;
-
-export const DeleteAccountTypo = styled(TextBold)`
-  text-align: center;
-  color: #dcccc2;
+export const ScrollContainer = styled.ScrollView`
+  padding-top: 10px;
+  padding-bottom: 10px;
 `;
 
-export const TermsOfUseTypo = styled(TextBold)`
-  text-align: center;
-  color: #dcccc2;
-`;
-
-export const ButtonContainer = styled.View`
+export const Image = styled.Image`
   width: 100%;
-  position: absolute;
-  left: 20px;
-  bottom: 12px;
-  gap: 6px;
-`;
-
-export const NavigationToBlockScreen = styled(BgBox)`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-export const TypoWrapper = styled.View``;
-export const NextButton = styled(Icon.Prev)`
-  transform: rotate(180deg);
+  aspect-ratio: 1;
+  margin-bottom: 10px;
 `;
